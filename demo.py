5609ff11190c84f509b20419bc00cea8e84fe530
Mattia Castellani <114669240+MarieCMDM@users.noreply.github.com>
2023-05-04 14:37:30 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,9 +1,8 @@
 import spi_led as led
 import time
 
-def demo():
+def blink():
     print("Starting demo... show grbw")
-    
     print("red")
     led.showColor([255, 0, 0])
     time.sleep(2)
@@ -24,5 +23,35 @@ def demo():
     led.showColor([0, 0, 0])
     print("Exiting demo...")
 
+def fade():
+    r = 255
+    g = 0
+    b = 0
+    speed = 0.3
+
+    while True:
+        while (r > 0):
+            led.showColor([r, g, b])
+            r -= 5
+            g += 5
+            time.sleep(speed)
+
+        while (g > 0):
+            led.showColor([r, g, b])
+            g -= 5
+            b += 5
+            time.sleep(speed)
+
+        while (b > 0):
+            led.showColor([r, g, b])
+            b -= 5
+            r += 5
+            time.sleep(speed)
+
 if __name__ == '__main__':
-    demo()
+    
+    try:
+        #blink()
+        fade()
+    except KeyboardInterrupt:
+        led.showColor([0, 0, 0])
