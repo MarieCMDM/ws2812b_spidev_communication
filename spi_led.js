942bf729ddbac995ff311653e1f72035998888db
Mattia Castellani <114669240+MarieCMDM@users.noreply.github.com>
2023-05-04 14:02:40 +0200

    Update spi_led.js

--- spi_led.js
+++ spi_led.js
@@ -1,62 +1,63 @@
-// On Test, only make the led green
-
 const spi = require('spi-device');
-const upvalue = 0b1110;
-const downvalue = 0b1000;
+const LED_NUM = 1;
 
-//rgb = [255, 255, 255]
-
+const args = process.argv;
+if (args.length == 5){
+  rgb = [args[2], args[3], args[4]]
+}else{
+  rgb = [0, 0, 0]
+}
 const spiPort = spi.open(0, 0, err => {
   // An SPI message is an array of one or more read+write transfers
   const message = [{
-    sendBuffer: Buffer.from([downvalue, downvalue, downvalue, downvalue, downvalue, downvalue, downvalue, downvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue]),
-    byteLength: 24,
+    sendBuffer: Buffer.from(messageParser(rgb)),
+    byteLength: 24 * LED_NUM,
     speedHz: 8000000 
   }];
 
   spiPort.transfer(message, (err, message) => {
     if (err) throw err;
-
-    console.log("fatto");
   });
 });
 
-
-/*
 function messageParser(rgb) {
-  // def color_parsing(rgb):
+  message = []
+  basemessage=colorParser(rgb);
+  i=1;
+  while (i<=LED_NUM){
+    message = message.concat(basemessage);
+    i++;
+  }
+  return message;
+};
+
+function colorParser(rgb){
   r = rgb[0]; 
   g = rgb[1];
   b = rgb[2];
-  // r, g, b = rgb
-
   //Create byte array with color data
   color_data = []
-  // color_data = []
-  for(int i=0, i<8, i++){
-    if ( (g >> (7-i)) & 0b1 ){
-      color_data.append(0b1110)
-    }else{
-      color_data.append(0b1000)
-    }
+  for(let i=0; i<8; i++){
+      if ( (g >> (7-i)) & 0b1 ){
+      color_data.push(0b1110);
+      }else{
+      color_data.push(0b1000);
+      }
   }
-  for(int i=0, i<8, i++){
-    if ( (r >> (7-i)) & 0b1 ){
-      color_data.append(0b1110)
-    }else{
-      color_data.append(0b1000)
-    }
+  for(let i=0; i<8; i++){
+      if ( (r >> (7-i)) & 0b1 ){
+      color_data.push(0b1110);
+      }else{
+      color_data.push(0b1000);
+      }
   }
-  for(int i=0, i<8, i++){
-    if ( (b >> (7-i)) & 0b1 ){
-      color_data.append(0b1110)
-    }else{
-      color_data.append(0b1000)
-    }
+  for(let i=0; i<8; i++){
+      if ( (b >> (7-i)) & 0b1 ){
+      color_data.push(0b1110);
+      }else{
+      color_data.push(0b1000);
+      }
   }
-
-  // return color_data
-
-  console.log("alc");
+  return color_data;
 };
-*/
+
