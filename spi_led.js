5d574ea01c8771252a7d0a5533b5197372c34ff6
Mattia Castellani <114669240+MarieCMDM@users.noreply.github.com>
2023-05-04 13:17:03 +0200

    Update spi_led.js

--- spi_led.js
+++ spi_led.js
@@ -1,19 +1,18 @@
 // On Test, only make the led green
 
 const spi = require('spi-device');
-const upvalue = 0b1110//0b11111100;
-const downvalue = 0b1000//0b10000000;
+const upvalue = 0b1110;
+const downvalue = 0b1000;
 
-// The MCP3008 is on bus 0 and it's device 0
+//rgb = [255, 255, 255]
+
 const spiPort = spi.open(0, 0, err => {
   // An SPI message is an array of one or more read+write transfers
   const message = [{
-    sendBuffer: Buffer.from([upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue]),
-    byteLength: 4,
+    sendBuffer: Buffer.from([downvalue, downvalue, downvalue, downvalue, downvalue, downvalue, downvalue, downvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue, upvalue]),
+    byteLength: 24,
     speedHz: 8000000 
   }];
-
-  if (err) throw err;
 
   spiPort.transfer(message, (err, message) => {
     if (err) throw err;
@@ -21,3 +20,43 @@ const spiPort = spi.open(0, 0, err => {
     console.log("fatto");
   });
 });
+
+
+/*
+function messageParser(rgb) {
+  // def color_parsing(rgb):
+  r = rgb[0]; 
+  g = rgb[1];
+  b = rgb[2];
+  // r, g, b = rgb
+
+  //Create byte array with color data
+  color_data = []
+  // color_data = []
+  for(int i=0, i<8, i++){
+    if ( (g >> (7-i)) & 0b1 ){
+      color_data.append(0b1110)
+    }else{
+      color_data.append(0b1000)
+    }
+  }
+  for(int i=0, i<8, i++){
+    if ( (r >> (7-i)) & 0b1 ){
+      color_data.append(0b1110)
+    }else{
+      color_data.append(0b1000)
+    }
+  }
+  for(int i=0, i<8, i++){
+    if ( (b >> (7-i)) & 0b1 ){
+      color_data.append(0b1110)
+    }else{
+      color_data.append(0b1000)
+    }
+  }
+
+  // return color_data
+
+  console.log("alc");
+};
+*/
